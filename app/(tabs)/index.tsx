3d49fc25d3e3242b3f44d2a7e3825d8832c78304
Jake Carpenter <jake@jakecarpenter.dev>
2025-07-29 17:53:03 -0600

    Add basic usage of BottomSheet

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,37 @@
 import { Stack } from 'expo-router'
 
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
+import BottomSheet, { BottomSheetBackdrop, BottomSheetView } from '@gorhom/bottom-sheet'
+import { useState } from 'react'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { Button } from '~/components/Button'
 import { ScreenContent } from '~/components/ScreenContent'
 
 export default function Home() {
+  const [visible, setVisible] = useState(false)
+
   return (
-    <>
+    <GestureHandlerRootView>
       <Stack.Screen options={{ title: 'Tab One' }} />
       <View style={styles.container}>
         <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
+        <Button title="Open bottom sheet" onPress={() => setVisible(true)} />
       </View>
-    </>
+      {visible && (
+        <BottomSheet
+          snapPoints={['50%', '90%']}
+          enableDynamicSizing={false}
+          index={0}
+          backdropComponent={BottomSheetBackdrop}
+          onClose={() => setVisible(false)}
+          enablePanDownToClose>
+          <BottomSheetView style={styles.container}>
+            <Text>Hello, World!</Text>
+          </BottomSheetView>
+        </BottomSheet>
+      )}
+    </GestureHandlerRootView>
   )
 }
 
